18e8b506d897a4750cb485d1de15502aabc11e83
Karen Boyakhchyan <karenboyakhchyan20@gmail.com>
2021-12-25 14:09:02 +0400

    added homework 1

--- flat.js
+++ flat.js
@@ -1,16 +1,20 @@
-const arr = [1,2,3,[2,5,[8],50,[25,[70],],4],[8]];
+const arr = [1, 2, 3, [2, 5, [8], 50, [25, [70]], 4], [8]];
 
-Array.prototype.myFlat = function(depth = 1) {
-    const newarr = [];
-    const removeBrackets = (arr, dp) => {
-        for(let i=0; i<arr.length; i++) {
-            if(Array.isArray(arr[i]) && dp){
-                removeBrackets(arr[i], dp - 1)
-            }else{
-                newarr.push(arr[i]);
+Array.prototype.myFlat = function (depth = 1) {
+    try {
+        const newarr = [];
+        const removeBrackets = (arr, dp) => {
+            for (let i = 0; i < arr.length; i++) {
+                if (Array.isArray(arr[i]) && dp) {
+                    removeBrackets(arr[i], dp - 1)
+                } else {
+                    newarr.push(arr[i]);
+                }
             }
-        }
-    };
-    removeBrackets(this, depth);
-    return newarr;
+        };
+        removeBrackets(this, depth);
+        return newarr;
+    } catch (e) {
+        console.log("Something went wrong...!", e);
+    }
 };
--- map.js
+++ map.js
@@ -1,9 +1,13 @@
 
-Array.prototype.myMap = function(callback) {
-    const newarr = [];
-    for(let i=0; i<this.length; i++) {
-        newarr.push(callback(this[i], i, this));
-    };
-    return newarr;
+Array.prototype.myMap = function (callback) {
+    try {
+        const newarr = [];
+        for (let i = 0; i < this.length; i++) {
+            newarr.push(callback(this[i], i, this));
+        };
+        return newarr;
+    } catch (e) {
+        console.log("Something went wrong...!", e);
+    }
 };
 
