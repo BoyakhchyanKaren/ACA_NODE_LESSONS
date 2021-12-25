371696129ae380681825d2639c2197739b151e8f
Karen Boyakhchyan <87094141+BoyakhchyanKaren@users.noreply.github.com>
2021-12-25 14:15:17 +0400

    Update map.js

--- map.js
+++ map.js
@@ -1,4 +1,13 @@
+//explaining how array map function works...!
+/*
 
+(method) Array<number | (number | (number | number[])[])[]>.map<any>(callbackfn: (value: number | (number | (number | number[])[])[], index: number, array: (number | (number | (number | number[])[])[])[]) => any, thisArg?: any): any[]
+Calls a defined callback function on each element of an array, and returns an array that contains the results.
+
+@param callbackfn — A function that accepts up to three arguments. The map method calls the callbackfn function one time for each element in the array.
+
+@param thisArg — An object to which the this keyword can refer in the callbackfn function. If thisArg is omitted, undefined is used as the this value.
+*/
 Array.prototype.myMap = function (callback) {
     try {
         const newarr = [];
