aa43874e4d877932cf244d95258b28bbebd98777
Karen Boyakhchyan <87094141+BoyakhchyanKaren@users.noreply.github.com>
2021-12-25 14:13:57 +0400

    Update flat.js

--- flat.js
+++ flat.js
@@ -1,3 +1,10 @@
+//explaining how array flat function works...!
+/*
+flat(depth?: 1): (number | (number | number[])[])[]
+The maximum recursion depth
+Returns a new array with all sub-array elements concatenated into it recursively up to the specified depth.
+*/
+
 const arr = [1, 2, 3, [2, 5, [8], 50, [25, [70]], 4], [8]];
 
 Array.prototype.myFlat = function (depth = 1) {
